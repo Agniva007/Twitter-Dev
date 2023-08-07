fa4712d477723adb354c62250746eb143ce51611
Agniva Das <agnivadas007@gmail.com>
2023-08-07 15:20:03 +0530

    added authentication using passport-jwt

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import passport from 'passport';
 
 import { connect } from './config/database.js';
+
+import { passportAuth } from './config/jwt-middleware.js';
+
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(passport.initialize());
+passportAuth(passport);
 
 import apiRoutes from './routes/index.js';
 import UserRepository from './repository/user-repository.js';
